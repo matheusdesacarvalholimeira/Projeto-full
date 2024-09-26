8056edf9439fb1b9a16dc8f88609becd9642c41b
Matheus <118131034+matheusdesacarvalholimeira@users.noreply.github.com>
2024-09-26 02:17:51 -0300

    first commit

---
+++ back/demo/src/main/java/com/example/demo/config/cors/Config.java
@@ -0,0 +1,24 @@
+package com.example.demo.config.cors;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permite CORS para todos os endpoints
+                        .allowedOrigins("http://localhost:3000") // Permite o domínio do front-end
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
+                        .allowedHeaders("*") // Permite todos os cabeçalhos
+                        .allowCredentials(true); // Permite envio de cookies e credenciais
+            }
+        };
+    }
+}
